1e8308e0756a5fb78c08aee97eb8b2f848065aa1
queenieliaw810910 <queenieliaw810910@gmail.com>
2017-04-27 16:20:49 +0800

    Update main.m

--- HW2/src/main.m
+++ HW2/src/main.m
@@ -1,15 +1,12 @@
 clear all;
 %Parameters
-sigma = 1.5;  k =0.04; localRadius= 20;
-
-sigma_smooth = 1; threshold = 50;
+sigma = 1.5;  k =0.04; localRadius= 20; threshold = 50;
 
 keypointNum = 400;
-%mode = 'strongest';
 
 margin = 50;
 
-
+% Get input images from input directory, and store them in dataset{}
 InputDir = 'csie/';
 files = dir(InputDir);
 files = files(4:end);
@@ -28,38 +25,29 @@ for i = 1:N
     end
 end
 
-% Get grayscale image
-Y = rgb2ycbcr(dataset{1});
-Y = Y(:,:,1);
+CornerDescription = {}
+for i=1:N
+	% Get grayscale image
+	disp(['For image',num2str(i),':'])
+	Y = rgb2ycbcr(dataset{i});
+	Y = Y(:,:,1);
+	Corner(i) = HarrisTop(Y, sigma, k, threshold, keypointNum, localRadius, margin);
+	featureSize = size(Corner(i).c,1);
+	CornerDescription{i} = FeatureDescriptor(Y,Corner(i));
+	figure, imagesc(dataset{i}), axis image, colormap(gray), hold on
+	plot(Corner(i).c,Corner(i).r ,'ys'), title('corners detected');
+end
 
-tic
-Corner_1 = HarrisTop(Y, sigma, k, threshold, keypointNum, localRadius, margin);%, mode);
-figure, imagesc(dataset{1}), axis image, colormap(gray), hold on
-plot(Corner_1.c,Corner_1.r ,'ys'), title('corners detected');
 
-CornerDescription_1 = FeatureDescriptor(Y,Corner_1);
-
-
-% Get grayscale image
-Y = rgb2ycbcr(dataset{2});
-Y = Y(:,:,1);
-
-tic
-Corner_2 = HarrisTop(Y, sigma, k, threshold, keypointNum, localRadius, margin);%, mode);
-figure, imagesc(dataset{2}), axis image, colormap(gray), hold on
-plot(Corner_2.c,Corner_2.r ,'ys'), title('corners detected');
-
-CornerDescription_2 = FeatureDescriptor(Y,Corner_2);
-
-[ point_matched point_distance] = knnMatch(CornerDescription_1,CornerDescription_2,10);
+[ point_matched point_distance] = knnMatch(CornerDescription{1},CornerDescription{2},10);
 %[matched point in 1_r , matched point in 1_c , matched point in 2_r, matched point in 2_c]
 
 %[ point_matched point_distance] = knnMatch(HarrisDiscriptor_1,HarrisDiscriptor_2,10);
 
 figure, imagesc(dataset{1}), axis image, colormap(gray), hold on
-plot(Corner_1.c,Corner_1.r ,'ys'), plot(point_matched(:,2),point_matched(:,1) ,'rs'),title('corners detected');
+plot(Corner(1).c,Corner(1).r ,'ys'), plot(point_matched(:,2),point_matched(:,1) ,'rs'),title('corners detected');
 figure, imagesc(dataset{2}), axis image, colormap(gray), hold on
-plot(Corner_2.c,Corner_2.r ,'ys'), plot(point_matched(:,4),point_matched(:,3) ,'rs'),title('corners detected');
+plot(Corner(2).c,Corner(2).r ,'ys'), plot(point_matched(:,4),point_matched(:,3) ,'rs'),title('corners detected');
 
 %figure, imagesc(dataset{1}), axis image, colormap(gray), hold on
 %plot(Corner_1.c,Corner_1.r ,'ys'), plot(point_matched(:,2),point_matched(:,1) ,'ys'),title('corners detected');
@@ -67,4 +55,3 @@ plot(Corner_2.c,Corner_2.r ,'ys'), plot(point_matched(:,4),point_matched(:,3) ,'
 %Corner = Harris_Laplace_fn(I,threshold);
 %Corner = Harris(I,sigma,k,threshold,localRadius,margin);
 %Corner = HarrisNMS(I,sigma,k,threshold,localRadius,keypointNum,margin);
-toc
