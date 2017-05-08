5d5ebf1e401fbed2a941af915563e04e2697e48f
Zhuohan Chen <pandachen0611@cmlab.csie.ntu.edu.tw>
2017-05-08 21:32:07 +0800

    Update imgWarp.m
    add alpha channel record

--- HW2/src/imgWarp.m
+++ HW2/src/imgWarp.m
@@ -1,8 +1,9 @@
 % use cylindrical coordination
-function [imgOut, descriptor] = imgWarp(img, description, f)
+function [imgOut, descriptor, alpha] = imgWarp(img, description, f)
 	[row, col, channel] = size(img);
 	col_warped = 2 * ceil(f * atan(col / 2 / f));
 	imgOut = zeros(row, col_warped, channel, 'uint8');
+	alpha = zeros(row, col_warped, 'uint8');
 	for r = 1:row
 		for c = 1:col
 			r_tmp = row / 2 - r;
@@ -12,6 +13,7 @@ function [imgOut, descriptor] = imgWarp(img, description, f)
 			cw = col_warped / 2 + cw;
 			rw = row / 2 - rw;
 			imgOut(rw, cw, :) = img(r, c, :);
+			alpha(rw, cw) = 255;
 		end
 	end
 	descriptor = description;
@@ -26,6 +28,7 @@ function [imgOut, descriptor] = imgWarp(img, description, f)
 		descriptor(i, 65) = rw;
 		descriptor(i, 66) = cw;
 	end
-	imgOut = imcrop(imgOut, [0, 0, col_warped-1, row]); % gonna fix this later
+	imgOut = imcrop(imgOut, [1, 1, col_warped-2, row]); % gonna fix this later
+	alpha = imcrop(alpha, [1, 1, col_warped-2, row]); % gonna fix this later
 	% imshow(imgOut);
 end
